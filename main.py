90c9fe41f52f3b96a913afbb2b67d572e4c2ce8f
jesse-prpic <13812@coderacademy.edu.au>
2024-08-22 23:52:26 +1000

    Added login route to authentication

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import os
 from flask import Flask
 
 from init import db, ma, bcrypt, jwt
-# from controllers.cli_controllers import db_commands
-# from controllers.auth_controller import auth_bp
+from controllers.cli_controllers import db_commands
+from controllers.auth_controllers import auth_bp
 
 def create_app():
     app = Flask(__name__)
@@ -15,8 +15,8 @@ def create_app():
     bcrypt.init_app(app)
     jwt.init_app(app)
 
-    # app.register_blueprint(db_commands)
-    # app.register_blueprint(auth_bp)
+    app.register_blueprint(db_commands)
+    app.register_blueprint(auth_bp)
     return app
 
 
